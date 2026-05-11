be829e21ed1b9eb7e3dddf6bcfcf0ab6e603b8f6
Tomoya Yahiro <145628597+k22rs142@users.noreply.github.com>
2026-05-11 12:41:06 +0900

    変数、条件分岐、繰り返し

---
+++ abc.rb
@@ -0,0 +1,28 @@
+puts "Hello, abc!"
+
+#条件分岐
+score = 80
+if score >= 90
+  #x = "SCORE" + score.to_s()
+  x = "SCORE:S #{score}"
+  puts x
+elsif score >= 80
+  x = "SCORE:A #{score}"
+elsif score >= 70
+  x = "SCORE:B #{score}"
+end
+puts x
+
+#繰り返し(while)
+a = 1
+while a <= 10
+  print "#{a},"
+  a += 1
+end
+puts
+
+#繰り返し(for)
+5.times do [i]
+  print "#{i}*"
+end
+puts
--- hello.rb
+++ hello.rb
@@ -1,3 +1,20 @@
 #! /usr/bin/env ruby
 
-puts "Hello, World!"
+puts ("Hello, World!")
+
+
+text = "test"
+puts text
+
+fruits = ["apple","grape","orange"]
+puts fruits[2]
+puts fruits
+
+a = 5
+puts a
+puts a.to_s()
+
+#変数と演算子
+#条件分岐と繰り返し
+#配列と参照
+#クラスとメソッド
