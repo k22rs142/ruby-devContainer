7b291ff765b2fb466ae0be7228ec4de1b1655e47
Tomoya Yahiro <145628597+k22rs142@users.noreply.github.com>
2026-07-06 12:35:24 +0900

    レスポンスにHTTTPヘッダを追加
    コンテンツをHTML化

--- server1.rb
+++ server1.rb
@@ -2,15 +2,29 @@
 require 'socket'
 
 def server s
+  #HTTP/1.0 として正しく返答
+  #1行目 HTTP/1.0 200 OK
+  #2行目 Content-Type text/html
+  #3行目 空行
+  #4行目 コンテンツ
+  #最後 ソケットをクローズ
   cmd, path, ver = s.gets.split(" ")
   # cmd = line.split(" ")
   if path == "/"
+    s.print "HTTP/1.0 200 OK\r\n"  
+    s.print "Content-Type: text/html\r\n"  
+    s.print "\r\n"  
     pp 'INDEX'
-    s.puts "index"
+    s.puts "<h1>index</h1>"
   else
     pp "OTHER"
-    s.puts 'other'
+    s.puts '<h1>other</h1>'
   end
+          
+  
+   
+
+
   # while line=s.gets
   #   pp line
   #   s.puts line
