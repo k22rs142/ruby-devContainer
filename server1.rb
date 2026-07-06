639a55c2e933a96e7e92712dd4b50726ac651633
Tomoya Yahiro <145628597+k22rs142@users.noreply.github.com>
2026-07-06 11:40:16 +0900

    PSTHを一応見る

--- server1.rb
+++ server1.rb
@@ -2,11 +2,20 @@
 require 'socket'
 
 def server s
-  while line=s.gets
-    pp line
-    s.puts line
-    break if line == "\r\n"
+  cmd, path, ver = s.gets.split(" ")
+  # cmd = line.split(" ")
+  if path == "/"
+    pp 'INDEX'
+    s.puts "index"
+  else
+    pp "OTHER"
+    s.puts 'other'
   end
+  # while line=s.gets
+  #   pp line
+  #   s.puts line
+  #   break if line == "\r\n"
+  # end
    s.close
 end
 
@@ -17,6 +26,6 @@ while true
   s = gs.accept
   Thread.new do #新しいスレッド処理を行う(並列処理?)
     server s
-    sleep 10
+    #sleep 10
   end
 end
